ef38e587b24fadb4ed31b7730f17e0d71aa044fb
TrimokRu <prokirik@gmail.com>
2022-07-12 21:08:24 +0300

    Initialization commit

---
+++ app/src/main/java/ru/trimok/notes/MainActivity.kt
@@ -0,0 +1,15 @@
+package ru.trimok.notes
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import ru.trimok.notes.databinding.ActivityMainBinding
+
+class MainActivity : AppCompatActivity() {
+
+    private lateinit var binding: ActivityMainBinding
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+    }
+}
